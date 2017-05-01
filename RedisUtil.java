6a09021ebf58082796d4077d03ae40b49b51884c
chushang2015 <m15872929877@163.com>
2017-05-01 10:38:57 +0800

    获取redis连接的工具类
    通过这个工具类可以从redis的连接池中获取redis的连接，不要忘记使用完后将连接还给连接池

--- RedisUtil.java
+++ RedisUtil.java
@@ -1,3 +1,4 @@
+﻿package cn.redis.demo1;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -6,19 +7,28 @@ import redis.clients.jedis.JedisPoolConfig;
 /**
  * 
  * @author Perfect Is Shit
+ *	Redis的工具类，用来获取redis的连接
  */
 public final class RedisUtil {
+	//redis服务器的ip
 	private static String ipAddress="127.0.0.1";
+	//redis服务器的端口
 	private static int port=6379;
+	//redis的访问密码
 	private static String password="123456";
+	//redis连接池可连接实例的最大数目
 	private static int max_active=1024;
+	//redis连接池最大可空闲的数目
 	private static int max_idle=200;
+	//获取redis连接的最大等待时间，超过这个事件就会抛异常
 	private static int max_wait=10000;
 	private static int timeout=1000;
 	
 	 private static boolean TEST_ON_BORROW = true;
+	//redis连接池
 	private static JedisPool jedisPool=null;
 	
+	//初始化连接池
 	static{
 		JedisPoolConfig config=new JedisPoolConfig();
 		config.setMaxTotal(max_active);
@@ -29,6 +39,7 @@ public final class RedisUtil {
 	}
 	
 	/**
+	 * 获取jedis连接的方法
 	 */
 	public synchronized static  Jedis getRedisConnection(){
 		if(jedisPool!=null){
@@ -39,6 +50,7 @@ public final class RedisUtil {
 	}
 	
 	/**
+	 * 释放jedis连接
 	 */
 	public static void returnResource(final Jedis jedis){
 		if(jedis!=null){
